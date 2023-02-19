08e27c1fd08fb4d3018277860cc440d736cbd4f5
OranFrydman <oranfry@post.bgu.ac.il>
2023-02-19 12:28:01 +0200

    branch 2 changes

--- GitClass.py
+++ GitClass.py
@@ -1,10 +1,12 @@
 print("My Secret  is 1234")
 
 print("Nice Secrert!")
-def plus (a,b):
+def plus1 (a,b):
     return a+b
 a=5
 b=7
-print(plus(a,b))
+print(plus1(a,b))
 
 print("okay the func works")
+
+print("Lets see now if its all okay")
