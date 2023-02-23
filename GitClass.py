63470ebc9b79b68423b9cdb4056668ddbf156dee
OranFrydman <oranfry@post.bgu.ac.il>
2023-02-23 18:02:42 +0200

    Delete all for OR

--- GitClass.py
+++ GitClass.py
@@ -1,14 +1,3 @@
-print("My Secret  is 1234")
 
-print("Nice Secrert!")
 
-def plus1 (a,b):
-    return a+b
-a=5
-b=7
-print(plus1(a,b))
-
-print("okay the func works")
-
-print("Lets see now if its all okay")
-
+print("Hello Or How are you")
